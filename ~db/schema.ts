57eeb23bbff4af69351dab2be4f91af13acfe73d
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-06-16 22:09:01 -0500

    feat: remove `not null` constraint when providing default value, and add updateAt column to existing tables.

--- ~db/schema.ts
+++ ~db/schema.ts
@@ -22,14 +22,17 @@ export const difficulties = pgEnum('difficulty', [
 export const courses = pgTable('courses', {
   id: uuid('id').primaryKey().default(sql`gen_random_uuid()`),
   title: varchar('title', { length: 50 }).notNull(),
-  headline: varchar('headline', { length: 256 }).notNull(),
-  description: text('description').notNull(),
-  price: numeric('price', { precision: 15, scale: 18 }).notNull(),
+  headline: varchar('headline', { length: 256 }).default(''),
+  description: text('description').default(''),
+  price: numeric('price', { precision: 15, scale: 18 }).default(
+    sql`0.0::numeric`,
+  ),
   difficulty: difficulties('difficulty').default('beginner'),
   isPublished: boolean('isPublished').default(false),
   isPaid: boolean('is_paid').default(true),
   isActive: boolean('is_active').default(true),
   createdAt: timestamp('created_at').default(sql`now()`),
+  updatedAt: timestamp('updated_at'),
 });
 
 export const modules = pgTable('modules', {
@@ -38,9 +41,10 @@ export const modules = pgTable('modules', {
     .notNull()
     .references(() => courses.id),
   title: varchar('title', { length: 50 }).notNull(),
-  summary: text('summary').notNull(),
+  summary: text('summary').default(''),
   isActive: boolean('is_active').default(true),
   createdAt: timestamp('createdAt').default(sql`now()`),
+  updatedAt: timestamp('updatedAt'),
 });
 
 export const lessons = pgTable('lessons', {
@@ -49,10 +53,11 @@ export const lessons = pgTable('lessons', {
     .notNull()
     .references(() => modules.id),
   title: varchar('title', { length: 50 }).notNull(),
-  videoUrl: varchar('video_url', { length: 256 }).notNull(),
+  videoUrl: varchar('video_url', { length: 256 }).default(''),
   isActive: boolean('is_active').default(true),
-  createdAt: timestamp('createdAt').default(sql`now()`),
   isPaid: boolean('is_paid').default(true),
+  createdAt: timestamp('createdAt').default(sql`now()`),
+  updatedAt: timestamp('updatedAt'),
   nextLessonId: integer('next_lesson_id').references(
     (): AnyPgColumn => lessons.id,
   ),
